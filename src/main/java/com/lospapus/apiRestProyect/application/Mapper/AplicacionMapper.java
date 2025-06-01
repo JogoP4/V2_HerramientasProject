0fc4cdce34b2224711322c714113e253227aa607
HoldenAndy <kaziroz09@gmail.com>
2025-06-01 17:11:20 -0500

    SCRUM-101 feat: Se hizo la conexión a la base de datos, ahora se puede guardar un usuario en la base de datos con la contraseña encriptada, gracias al jwt

--- src/main/java/com/lospapus/apiRestProyect/application/Mapper/AplicacionMapper.java
+++ src/main/java/com/lospapus/apiRestProyect/application/Mapper/AplicacionMapper.java
@@ -17,28 +17,5 @@ public class AplicacionMapper {
         UsuarioMapper.updateDomainFromDto(requestDTO, usuarioDomain);
     }
 
-    // Mapeo para Nota
-    public NotaResponseDTO toNotaResponseDTO(Nota notaDomain) {
-        return NotaMapper.toNotaResponseDTO(notaDomain);
-    }
-    public Nota toDomain(RegistrarNotaRequestDTO requestDTO) {
-        return NotaMapper.toDomain(requestDTO);
-    }
-
-    //Mapeo para asignatura
-    public AsignaturaResponseDTO toAsignaturaResponseDTO(Asignatura asignaturaDomain) {
-        return AsignaturaMapper.toAsignaturaResponseDTO(asignaturaDomain);
-    }
-    public Asignatura toDomain(CrearAsignaturaRequestDTO requestDTO) {
-        return AsignaturaMapper.toDomain(requestDTO);
-    }
-
-    //Mapeo para Curso
-    public CursoResponseDTO toCursoResponseDTO(Curso cursoDomain){
-        return CursoMapper.toCursoResponseDTO(cursoDomain);
-    }
-    public Curso toDomain(CrearCursoRequestDTO requestDTO){
-        return CursoMapper.toDomain(requestDTO);
-    }
 
 }

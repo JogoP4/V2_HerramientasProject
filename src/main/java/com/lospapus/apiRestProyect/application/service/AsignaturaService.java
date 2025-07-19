7b5c8aa4bc67e1ef839ebc882e91b967a3046acf
Josué Palacios <palacioscheroj@gmail.com>
2025-07-19 17:32:38 -0500

    Update AsignaturaService.java

--- src/main/java/com/lospapus/apiRestProyect/application/service/AsignaturaService.java
+++ src/main/java/com/lospapus/apiRestProyect/application/service/AsignaturaService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class AsignaturaService {
     private final AsignaturaRepository asignaturaRepository;
     private final AplicacionMapper mapper;

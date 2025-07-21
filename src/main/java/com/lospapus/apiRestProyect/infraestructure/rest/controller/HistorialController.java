27b5312351ce06adde50ec926291613e344738a1
rengal3 <palacioscheroj@gmail.com>
2025-07-21 15:08:19 -0500

    feat-S60-Crear endpoint para obtener historial académico.

---
+++ src/main/java/com/lospapus/apiRestProyect/infraestructure/rest/controller/HistorialController.java
@@ -0,0 +1,14 @@
+package com.lospapus.apiRestProyect.infraestructure.rest.controller;
+
+import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+public class HistorialController {
+    
+//@GetMapping("/historial/{usuarioId}")    
+}
+
+
+//Commit-S60-Crear endpoint para obtener historial académico.
